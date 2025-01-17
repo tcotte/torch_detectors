Add augmentations + fix bug mAP computation

src/torch_implementation/detect.py
@@ -1,4 +1,5 @@
 import time
+import typing
 
 import albumentations as A
 import cv2
@@ -16,7 +17,8 @@ class_mapping = {
 }
 
 IMAGE_SIZE = (1024, 1024)
-
+MODEL_PATH: typing.Final[str] = r'C:\Users\tristan_cotte\PycharmProjects\yolov8_keras\models\run_01_17_2025_09_00_22\latest.pth'
+BATCH_SIZE = 4
 # yolo.to('cuda')
 
 transform = A.Compose([
@@ -35,7 +37,7 @@ train_dataset = PascalVOCDataset(
 
 data_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=4,
+    batch_size=BATCH_SIZE,
     shuffle=True,
     # collate_fn=train_dataset.collate_fn
     collate_fn=collate_fn
@@ -47,24 +49,25 @@ model = create_retinanet_model(num_classes=len(class_mapping))
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 # device = torch.device('cpu')
 
-model.load_state_dict(torch.load('C:/Users/tristan_cotte/Downloads/latest.pth', weights_only=True))
+model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
 model.to(device)
 model.eval()
 
 if __name__ == "__main__":
-    MIN_CONFIDENCE = 0.2
+    MIN_CONFIDENCE = 0.25
     MIN_IOU_THRESHOLD = 0.2
 
     unorm = UnNormalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
-    metric = MeanAveragePrecision(iou_type="bbox")
+    metric_by_batch = MeanAveragePrecision(iou_type="bbox")
+    metric_onebyone = MeanAveragePrecision(iou_type="bbox")
 
-    for i in range(4):
+    for i in range(2):
         images, targets = next(iter(data_loader))
         images = list(image.to(device) for image in images)
 
         targets_gpu = []
         for j in range(len(targets)):
-            targets_gpu.append({k: v.to(device=device, non_blocking=True) for k, v in targets[0].items()})
+            targets_gpu.append({k: v.to(device=device, non_blocking=True) for k, v in targets[j].items()})
 
         start_predictions = time.time()
         with torch.no_grad():
@@ -76,9 +79,14 @@ if __name__ == "__main__":
             iou_threshold=MIN_IOU_THRESHOLD,
             confidence_threshold=MIN_CONFIDENCE)
 
-        metric.update(processed_predictions, targets_gpu)
-        print(metric.compute())
-        metric.reset()
+        for index, prediction in enumerate(processed_predictions):
+            metric_onebyone.update([prediction], [targets_gpu[index]])
+            print(f'Metric on image {index + i*BATCH_SIZE}: {metric_onebyone.compute()}')
+            metric_onebyone.reset()
+
+        metric_by_batch.update(processed_predictions, targets_gpu)
+        print(f'Metric by batch: {metric_by_batch.compute()}')
+        metric_by_batch.reset()
 
         for index, (img, pred) in enumerate(zip(images, processed_predictions)):
             img = unorm(img)
@@ -92,7 +100,7 @@ if __name__ == "__main__":
 
             pred_scores = pred['scores'].detach().cpu().numpy()
 
-            # metric.update([pred], [targets_gpu[index]])
+            # metric_by_batch.update([pred], [targets_gpu[index]])
 
             for bbox_index in range(len(pred_scores)):
                 bbox = pred['boxes'][bbox_index].detach().cpu().numpy()
@@ -102,6 +110,7 @@ if __name__ == "__main__":
                 cv2.putText(img, f'{pred_scores[bbox_index]:.2f}', (int(bbox[0] - 10), int(bbox[1] - 10)),
                             color=(0, 0, 255), thickness=1, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5)
 
+            plt.title(f'Image {index + i*BATCH_SIZE}')
             plt.imshow(img)
             plt.show()
 